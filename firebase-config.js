4814a008175955af78eda126376842683d3776d8
sunnysidecreativeco <94397221+sunnysidecreativeco@users.noreply.github.com>
2024-04-11 02:15:40 +0000

    firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,25 @@
-// import firebase from "firebase/app";
-// import "firebase/auth";
-// import "firebase/firestore";
-// import "firebase/storage";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC-SL5BdHNW4D8aSM6lQXt-GD5E0MF46nk",
-//   authDomain: "planet-music-98c38.firebaseapp.com",
-//   projectId: "planet-music-98c38",
-//   storageBucket: "planet-music-98c38.appspot.com",
-//   messagingSenderId: "981119710304",
-//   appId: "1:981119710304:web:fc501eea4ad0b0f4aeff05"
+const firebaseConfig = {
+  apiKey: "AIzaSyC-SL5BdHNW4D8aSM6lQXt-GD5E0MF46nk",
+  authDomain: "planet-music-98c38.firebaseapp.com",
+  projectId: "planet-music-98c38",
+  storageBucket: "planet-music-98c38.appspot.com",
+  messagingSenderId: "981119710304",
+  appId: "1:981119710304:web:fc501eea4ad0b0f4aeff05"
 
-// };
+};
 
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// // Initialize services
-// const auth = firebase.auth();
-// const db = firebase.firestore();
-// const storage = firebase.storage();
+// Initialize services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
 
-// // Export services
-// export { auth, db, storage };
+// Export services
+export { auth, db, storage };
