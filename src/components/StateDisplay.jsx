a45d47c86c258b375612a31a1421417bc8d5e98f
sunnysidecreativeco <94397221+sunnysidecreativeco@users.noreply.github.com>
2024-04-11 01:37:21 +0000

    Firebase install and test

--- src/components/StateDisplay.jsx
+++ src/components/StateDisplay.jsx
@@ -1,13 +1,51 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
+import { db, auth, storage } from './firebase-config';
+import { doc, getDoc } from 'firebase/firestore';
 
 function StateDisplay() {
     const [count, setCount] = useState(0);
+    const [giveawayItem, setGiveawayItem] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    //Connect to the db
+    useEffect(() => {
+        const loadGiveaway = async () => {
+            setLoading(true); // Start loading
+            const docRef = doc(db, 'giveaway', 'giveawayStatus');
+            const docSnap = await getDoc(docRef);
+    
+            if (docSnap.exists()) {
+                const currentGiveawayItem = docSnap.data().item;
+                setGiveawayItem(currentGiveawayItem);
+            } else {
+                console.log("No such document!");
+            }
+            setLoading(false); // Data loaded
+        };
+    
+        loadGiveaway();
+    }, []);
+    //Run the query
+    //Return the result
+
+
 
     return (
         <div>
-            <p>Current Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <div>
+                <p>Current Count: {count}</p>
+                <button onClick={() => setCount(count + 1)}>Increment</button>
+            </div>
+    
+            {loading ? (
+                <p>Loading giveaway item...</p>
+            ) : (
+                <div>
+                    <p>Giveaway Item: {giveawayItem}</p>
+                </div>
+            )}
         </div>
+
     );
 }
 
