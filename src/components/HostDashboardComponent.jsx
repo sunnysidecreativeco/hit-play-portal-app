316e0f59d5b8cbfdf9b66c424aadaaceaa8a4744
sunnysidecreativeco <94397221+sunnysidecreativeco@users.noreply.github.com>
2024-04-19 21:43:04 +0000

    storage fix

--- src/components/HostDashboardComponent.jsx
+++ src/components/HostDashboardComponent.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'preact/hooks';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
-import { db } from '../../firebase-config';
+import { db, storage } from '../../firebase-config';
 import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { ref as storageRef, getDownloadURL } from 'firebase/storage';
 import '../styles/styles.css';
 
 function HostDashboardComponent() {
