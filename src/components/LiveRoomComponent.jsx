8e32d2d894d0ac908e46cf5b685a640354e65090
sunnysidecreativeco <94397221+sunnysidecreativeco@users.noreply.github.com>
2024-04-19 23:36:08 +0000

    Play and pause buttons

--- src/components/LiveRoomComponent.jsx
+++ src/components/LiveRoomComponent.jsx
@@ -403,9 +403,19 @@ function LiveRoomComponent() {
                     {nowPlaying.length > 0 ? nowPlaying.map(song => (
                         <div key={song.id} className="song-item">
                             <p>{song.songName} by {song.artistName}</p>
+                            {song.songLink && ( // Comment: Displaying Spotify link
+                                    <a href={song.songLink} target="_blank" rel="noopener noreferrer">
+                                        <img src="../../images/Spotify-Icon-1.0.png" alt="Spotify" style={{ width: '24px', height: '24px', marginLeft: '10px' }} />
+                                    </a>
+                                )}
                             <audio ref={audioRef} onTimeUpdate={handleTimeUpdate} onLoadedMetadata={handleLoadedMetadata} onEnded={() => setIsPlaying(false)} />
                             <div>
-                                <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
+                                <button onClick={togglePlay} style={{ border: 'none', background: 'none', padding: 0 }}>
+                                    {isPlaying 
+                                        ? <img src="../../images/Pause-Icon-1.0.png" alt="Pause" style={{ width: '25px', height: '25px' }} />
+                                        : <img src="../../images/Play-Icon-1.0.png" alt="Play" style={{ width: '25px', height: '25px' }} />
+                                    }
+                                </button>
                                 <input type="range" min="0" max={duration || 1} value={currentTime} onChange={(e) => {
                                     audioRef.current.currentTime = e.target.value;
                                     setCurrentTime(e.target.value);
@@ -415,11 +425,7 @@ function LiveRoomComponent() {
                                     <span> / </span>
                                     <span>{Math.floor(duration / 60)}:{('0' + Math.floor(duration % 60)).slice(-2)}</span>
                                 </div>
-                                {song.songLink && ( // Comment: Displaying Spotify link
-                                    <a href={song.songLink} target="_blank" rel="noopener noreferrer">
-                                        <img src="../../images/Spotify-Icon-1.0.png" alt="Spotify" style={{ width: '24px', height: '24px', marginLeft: '10px' }} />
-                                    </a>
-                                )}
+                                
                             </div>
                         </div>
                     )) : <p>No songs currently playing.</p>}
