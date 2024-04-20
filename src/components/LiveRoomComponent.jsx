ade26548c23febc800b00e8dcc866dec03197a8d
sunnysidecreativeco <94397221+sunnysidecreativeco@users.noreply.github.com>
2024-04-20 01:41:20 +0000

    Stying

--- src/components/LiveRoomComponent.jsx
+++ src/components/LiveRoomComponent.jsx
@@ -174,7 +174,8 @@ function LiveRoomComponent() {
         width: '24px', 
         height: '24px', 
         marginLeft: '10px',
-        
+        marginTop: '-35px',
+        verticalAlign: 'middle',
     };
     
     
